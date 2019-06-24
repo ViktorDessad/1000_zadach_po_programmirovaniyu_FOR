5e39d39ebe71b3dac2da080d4c3294ea76e249e2
Віктор Шереш <sheregi1000@gmail.com>
2019-06-24 21:41:10 +0300

    Add project files.

---
+++ 1000_zadach_po_programmirovaniyu_FOR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_zadach_po_programmirovaniyu_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
