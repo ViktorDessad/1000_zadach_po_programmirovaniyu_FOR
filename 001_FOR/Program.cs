87bd539531d33e0c8dec9330c6ecac82cd0788c6
Віктор Шереш <sheregi1000@gmail.com>
2019-06-24 21:49:55 +0300

    Comment

---
+++ 001_FOR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < num1; i++)
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
